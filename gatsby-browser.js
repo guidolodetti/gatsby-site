04d9d3b1d157fdbdc48b9860bcfe83cdf25f0901
Guido Lodetti <guido.lode@gmail.com>
2019-01-23 15:50:37 +0100

    Added ApolloBoost

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,17 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from 'react'
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient from 'apollo-boost'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+})
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ApolloProvider client={client}>{element}</ApolloProvider>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { withApollo } from 'react-apollo'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
@@ -18,4 +19,4 @@ const IndexPage = () => (
   </Layout>
 )
 
-export default IndexPage
+export default withApollo(IndexPage)
